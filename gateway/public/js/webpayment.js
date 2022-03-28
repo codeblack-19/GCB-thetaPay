533a9c3ebcc44bbc55e6d304b6f004ad6bb79ecf
codeblack-19 <codeblack-19@users.noreply.github.com>
2022-03-28 15:57:24 +0000

    touched client side payment process

--- gateway/public/js/webpayment.js
+++ gateway/public/js/webpayment.js
@@ -1,32 +1,179 @@
-var formMessage = document.getElementById("_message");
-var interForm = document.getElementById('interPay');
-var sbtBtn = document.getElementsByClassName('submitBtn');
-var cancelBtn = document.getElementsByClassName('cancelBtn');
-var cardForm = document.getElementById('cardPayment');
+var formMessage, interForm, sbtBtn, cancelBtn, cardForm;
+var FmBody = {};
 
-// payment switch operation
+
+// display credit card pay form
 function displayCardForm(){
-    document.getElementsByClassName('choice_wp')[0].style.display = "none";
-    document.getElementsByClassName('theta-to-theta')[0].style.display = "none";
-    document.getElementsByClassName('cardPayment')[0].style.display = "block";
+    document.getElementsByClassName('selections')[0].innerHTML = `
+        <div class="cardPayment">
+                <p id="_message"></p>
+                <form id="cardPayment" method="post" action="">
+                    <div class="field-container">
+                        <label for="name">Name</label>
+                        <input id="name" maxlength="20" type="text" placeholder="e.g John Doe">
+                    </div>
+                    <div class="field-container">
+                        <label for="cardnumber">Card Number</label>
+                        <input id="cardnumber" type="number" pattern="[0-9]*" inputmode="numeric" maxlength="16" placeholder="XXXX XXXX XXXX XXXX">
+                    </div>
+                    <div class="field-container flexdt">
+                        <label for="expirationdate">Expiration (mm/yy)</label>
+                        <input id="expiry_mm" type="text" pattern="[0-9]*" maxlength="2" inputmode="numeric" placeholder="MM">
+                        <input id="expiry_yy" type="text" pattern="[0-9]*" maxlength="2" inputmode="numeric" placeholder="YY">
+                    </div>
+                    <div class="field-container">
+                        <label for="securitycode">CVV</label>
+                        <input id="securitycode" type="text" pattern="[0-9]*" maxlength="4" inputmode="numeric" placeholder="XXXX">
+                    </div>
+                    <div class="btns">
+                        <button type="submit" id="submitBtn">Submit</button>
+                        <button type="button" id="cancelBtn" onclick="resetPage()">Reset</button>
+                    </div>
+                </form>
+            </div>
+    `
+    cardForm = document.getElementById('cardPayment');
+    setElements();
+    cardForm.addEventListener('submit', (e) => submitCreditCard(e));
 }
 
+// display interpay form
 function displayInterForm(){
-    document.getElementsByClassName('choice_wp')[0].style.display = "none";
-    document.getElementsByClassName('theta-to-theta')[0].style.display = "block";
-    document.getElementsByClassName('cardPayment')[0].style.display = "none";
+    document.getElementsByClassName('selections')[0].innerHTML = `
+        <div class="theta-to-theta">
+                <p id="_message"></p>
+                <form id="interPay" method="post" action="">
+                    <input type="number" autocomplete="off" name="accountNo" maxlenth="10" placeholder="Account Number" />
+                    <input type="password" autocomplete="off" name="pinCode" maxlength="6" placeholder="Pin Code" />
+                    <div class="btns">
+                        <button type="submit" id="submitBtn">Submit</button>
+                        <button type="button" id="cancelBtn" onclick="resetPage()">Reset</button>
+                    </div>
+                </form>
+            </div>
+    `
+    interForm = document.getElementById('interPay');
+    setElements();
+    interForm.addEventListener('submit', (e) => submitInterForm(e));
 }
 
-function resetPage(){
-    // document.getElementsByClassName('choice_wp')[0].style.display = "flex";
-    // document.getElementsByClassName('theta-to-theta')[0].style.display = "none";
-    // document.getElementsByClassName('cardPayment')[0].style.display = "none";
+// set dom element to variables
+function setElements(){
+    formMessage = document.getElementById("_message");
+    sbtBtn = document.getElementById('submitBtn');
+    cancelBtn = document.getElementById('cancelBtn');
+}
 
+// reset payment page
+function resetPage(){
     location.reload();
 }
 
-// theta-to-theta Pay
-interForm?.addEventListener('submit', function(e){
-    e.preventDefault();
+// set form message => error & success
+const setformMessage = (type, message) => {
+    formMessage.setAttribute("class", type);
+    formMessage.innerText = message;
+    sbtBtn.style.display = "block";
+    cancelBtn.style.display = "block";
+}
 
-});
+// theta-to-theta Pay
+function submitInterForm(e){
+    e.preventDefault();
+    sbtBtn.style.display = "none";
+    cancelBtn.style.display = "none";
+
+
+    FmBody['accountNo'] = interForm.elements['accountNo'].value;
+    FmBody['pinCode'] = interForm.elements['pinCode'].value;
+    FmBody['signature'] = new URLSearchParams(window.location.search).get('signature');
+
+    if(FmBody.accountNo == ""){
+        return setformMessage("error", "Account Number is required");
+    }else if(FmBody.pinCode == ""){
+        return setformMessage("error", "Pin Code is required");
+    }else if(FmBody.pinCode.length == 0){
+        return setformMessage("error", "Pin Code must be 6 digits");
+    }else{
+        $.ajax({
+            type: "POST",
+            processData: false,
+            contentType: "application/json; charset=utf-8",
+            cache: false,
+            url: "/GCB-thetaPay/gateway/transactions/intertransfer",
+            data: JSON.stringify(FmBody),
+            dataType: 'json',
+            success: function () {
+                interForm.reset();
+                document.getElementsByTagName("main")[0].innerHTML = `
+                    <div class="verify_notice">
+                        <h3>Transaction Completed Succesfully</h3>
+                        <p class="check"><span>&#10003;</span></p>
+                        <a href="/GCB-thetaPay/gateway">
+                            <button>Go To Portal</button>
+                        </a>
+                    </div>
+                `
+            }, error: function (XMLHttpRequest){
+                setformMessage("error", XMLHttpRequest.responseJSON.error);
+            }
+        });
+    }
+};
+
+// credit card payment
+function submitCreditCard(e){
+    e.preventDefault();
+    sbtBtn.style.display = "none";
+    cancelBtn.style.display = "none";
+
+    FmBody['name'] = cardForm.elements['name'].value;
+    FmBody['cardNo'] = cardForm.elements['cardnumber'].value;
+    FmBody['expiry_mm'] = cardForm.elements['expiry_mm'].value;
+    FmBody['expiry_yy'] = cardForm.elements['expiry_yy'].value;
+    FmBody['cvv'] = cardForm.elements['securitycode'].value;
+    FmBody['signature'] = new URLSearchParams(window.location.search).get('signature');
+
+    if(FmBody.name == ""){
+        return setformMessage("error", "Card holder name is required");
+    }else if(FmBody.cardNo == ""){
+        return setformMessage("error", "Card number is required");
+    }else if(FmBody.cardNo.length != 16){
+        return setformMessage("error", "Card number must be 16 digits");
+    }else if(FmBody.expiry_mm == ""){
+        return setformMessage("error", "Expiration month required")
+    }else if(FmBody.expiry_yy == ""){
+        return setformMessage("error", "Expiration year required");
+    }else if(FmBody.expiry_mm.length != 2 || FmBody.expiry_yy.length != 2){
+        return setformMessage("error", "Invalid Expiration month or year");
+    }else if(FmBody.cvv == ""){
+        return setformMessage("error", "CVV is required");
+    }else if(FmBody.cvv.length != 4){
+        return setformMessage("error", "Invalid cvv number");
+    }else{
+        $.ajax({
+            type: "POST",
+            processData: false,
+            contentType: "application/json; charset=utf-8",
+            cache: false,
+            url: "/GCB-thetaPay/gateway/transactions/cardpayment",
+            data: JSON.stringify(FmBody),
+            dataType: 'json',
+            success: function () {
+                cardForm.reset();
+                document.getElementsByTagName("main")[0].innerHTML = `
+                    <div class="verify_notice">
+                        <h3>Transaction Completed Succesfully</h3>
+                        <p class="check"><span>&#10003;</span></p>
+                        <a href="/GCB-thetaPay/gateway">
+                            <button>Go To Portal</button>
+                        </a>
+                    </div>
+                `
+            },
+            error: function (XMLHttpRequest) {
+                setformMessage("error", XMLHttpRequest.responseJSON.error);
+            }
+        });
+    }
+}
--- gateway/views/transactions/paymentpage.php
+++ gateway/views/transactions/paymentpage.php
@@ -13,54 +13,17 @@
     <main class="content">
         <div class="choice_bx">
             <h4>Finish Payment via: <span id="_pmethod"></span> </h4>
-            <div class="choice_wp">
-                <div class="choice" id="thetaTotheta" onclick="displayInterForm()">
-                    <img src="../public/asserts/favicon.ico" />
-                    <p>theta-to-theta</p>
+            <div class="selections">
+                <div class="choice_wp">
+                    <div class="choice" id="thetaTotheta" onclick="displayInterForm()">
+                        <img src="../public/asserts/favicon.ico" />
+                        <p>theta-to-theta</p>
+                    </div>
+                    <div class="choice" id="bank_card" onclick="displayCardForm()">
+                        <img src="../public/asserts/visa_n.png" />
+                        <p>Credit Cards</p>
+                    </div>
                 </div>
-                <div class="choice" id="bank_card" onclick="displayCardForm()">
-                    <img src="../public/asserts/visa_n.png" />
-                    <p>Credit Cards</p>
-                </div>
-            </div>
-
-            <div class="theta-to-theta">
-                <p id="_message"></p>
-                <form id="interPay" method="post" action="paymentpage.php">
-                    <input type="number" autocomplete="off" name="accountNo" placeholder="Account Number" />
-                    <input type="password" autocomplete="off" name="pinCode" placeholder="Pin Code" />
-                    <div class="btns">
-                        <button type="submit" class="submitBtn">Submit</button>
-                        <button type="button" class="cancelBtn" onclick="resetPage()">Reset</button>
-                    </div>
-                </form>
-            </div>
-
-            <div class="cardPayment">
-                <p id="_message"></p>
-                <form id="cardPayment" method="post" action="paymentpage.php">
-                    <div class="field-container">
-                        <label for="name">Name</label>
-                        <input id="name" maxlength="20" type="text" placeholder="e.g John Doe">
-                    </div>
-                    <div class="field-container">
-                        <label for="cardnumber">Card Number</label>
-                        <input id="cardnumber" type="number" pattern="[0-9]*" inputmode="numeric" maxlength="16" placeholder="XXXX XXXX XXXX XXXX">
-                    </div>
-                    <div class="field-container flexdt">
-                        <label for="expirationdate">Expiration (mm/yy)</label>
-                        <input id="expiry_mm" type="text" pattern="[0-9]*" maxlength="2" inputmode="numeric" placeholder="MM">
-                        <input id="expiry_yy" type="text" pattern="[0-9]*" maxlength="2" inputmode="numeric" placeholder="YY">
-                    </div>
-                    <div class="field-container">
-                        <label for="securitycode">CVV</label>
-                        <input id="securitycode" type="text" pattern="[0-9]*" maxlength="4" inputmode="numeric" placeholder="XXXX">
-                    </div>
-                    <div class="btns">
-                        <button type="button" class="submitBtn">Submit</button>
-                        <button type="button" class="cancelBtn" onclick="resetPage()">Reset</button>
-                    </div>
-                </form>
             </div>
         </div>
     </main>
